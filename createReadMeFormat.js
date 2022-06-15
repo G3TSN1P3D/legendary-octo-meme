7d0372e94b1e5b52f2c27cc673d6f849490d6491
G3TSN1P3D <chava4flores@hotmail.com>
2022-06-15 12:41:43 -0400

    Latest update. Updated createReadMeFormat.js

--- createReadMeFormat.js
+++ createReadMeFormat.js
@@ -1,50 +1,50 @@
 function createReadMeFormat(data) {
   return `
-    # ${data.title}
+# ${data.title}
 
-    ## Description
-    
-    ${data.description}
+## Description
 
-    - ${data.motivation}
-    - ${data.why}
-    - ${data.problem}
-    - ${data.learnt}
-        
-    ## Table of Content 
+${data.description}
 
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    - [Features](#features)
-    - [How to Contribute](#contribute)
-    
-    ## Installation
-    
-    ${data.installation}
-    
-    ## Usage
-    
-    ${data.use}
-    
-    ## Credits
-    
-    ${data.collaborators}
-    
-    ## License
-    
-    ${data.license}
-        
-    ## Features
-    
-    ${data.features}
-    
-    ## How to Contribute
-    
-    [${data.title}](https://github.com/G3TSN1P3D/${data.title})
-    
-    `;
+- ${data.motivation}
+- ${data.why}
+- ${data.problem}
+- ${data.learnt}
+
+## Table of Content 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+- [How to Contribute](#contribute)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.use}
+
+## Credits
+
+${data.collaborators}
+
+## License
+
+${data.license}
+
+## Features
+
+${data.features}
+
+## How to Contribute
+
+[${data.title}](https://github.com/G3TSN1P3D/${data.title})
+
+`;
 }
 
 module.exports = createReadMeFormat;
